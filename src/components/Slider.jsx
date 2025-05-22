ecc6cfc7085ff64633825f4f1f7d59dbe53af1ea
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-05-22 15:15:43 +0600

    Slider Img and Title updated with design

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -24,7 +24,8 @@ export default function Slider({ slidesData }) {
       >
         {slidesData.map((slide) => (
           <SwiperSlide key={slide.id}>
-            <div className="bg-base-300 border border-base-300 w-full rounded-md shadow-2xl">
+            <div className="relative bg-base-300 border border-base-300 w-full rounded-md shadow-2xl">
+              <h1 className="absolute top-5 md:top-10 left-5 md:left-10 bg-[#B6B09F]/50 border-2 border-dotted border-[#EAE4D5] text-white p-1 md:p-3 rounded-md font-bold w-30 md:w-65 lg:w-100 text-xs md:text-xl lg:text-3xl z-10 text-center">{slide.title}</h1>
               <img className="w-full lg:h-120 h-80" src={slide.image} alt="" />
             </div>
           </SwiperSlide>
